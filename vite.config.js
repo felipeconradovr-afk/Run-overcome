ec618732d8a11e9d433f5ac63c722e94b51a6c66
Felipe Conrado <felipeconradovr@gmail.com>
2026-09-02 16:54:46 -0300

    Gera nova pasta www e envia para o GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Removemos a propriedade root para que ele use a raiz padrão onde o index.html já está
   build: {
-    outDir: 'dist',
+    outDir: 'www',
     emptyOutDir: true,
   }
 });
